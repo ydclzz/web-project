1805ccb1510d487335246445b7d651bd941df717
zejet <steven@Zejians-MacBook-Pro.local>
2017-08-16 15:44:25 -0400

    Login page

--- public/project/views/controller/login.controller.client.js
+++ public/project/views/controller/login.controller.client.js
@@ -5,7 +5,11 @@
 
     function loginController($location, $rootScope, userService) {
         var model = this;
+        model.flag = false;
         model.login = login;
+        model.toggle = toggle;
+        model.register = register;
+
         function init() {
         }
         init();
@@ -24,6 +28,40 @@
                     model.errorMessage = "wrong username or password";
                 });
 
+        }
+
+        function toggle() {
+            if(model.flag == true) {
+                model.flag = false;
+            } else {
+                model.flag = true;
+            }
+        }
+
+        function register(user) {
+            if (user.password === user.secondPassword) {
+                userService.findUserByUsername(user.username)
+                    .then(function (response) {
+                        if (!response.data || response.data === "0") {
+                            return userService.createUser(user);
+                        } else {
+                            model.errorRegisterMessage = "username already exist";
+                            return "exist";
+                        }
+                    })
+                    .then(function(response){
+                        if(response && response !== "exist"){
+                            user = response.data;
+                            $location.url("/user/" + user._id);
+                        }
+                        else if(response !== "exist"){
+                            model.errorRegisterMessage = response;//"something goes wrong";
+                        }
+                    });
+            } else {
+                model.errorRegisterMessage = "password do not match";
+                return;
+            }
         }
 
     }
