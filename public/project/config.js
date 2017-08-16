29d9c5052189102a542e267f8405b6b3fae91e19
liuchuhan <liu.chuh@husky.neu.edu>
2017-08-16 19:44:04 -0400

    profile

--- public/project/config.js
+++ public/project/config.js
@@ -5,9 +5,18 @@
 
     function configuration($routeProvider) {
         $routeProvider
+<<<<<<< Updated upstream
             .when("/search", {
                 templateUrl: "views/templates/search.html",
                 controller: "searchController",
+=======
+            .when("/", {
+                // templateUrl: "views/templates/search.html",
+                // controller: "searchController",
+                // controllerAs: "model"
+                templateUrl: "views/templates/login.html",
+                controller: "loginController",
+>>>>>>> Stashed changes
                 controllerAs: "model"
             })
             .when("/play", {
@@ -18,17 +27,28 @@
                 controller: "loginController",
                 controllerAs: "model"
             })
+<<<<<<< Updated upstream
             .when("/profile", {
                 templateUrl: "views/templates/user-home.html",
+=======
+            .when("/user/:uid", {
+                templateUrl: "views/templates/userHome.html",
+>>>>>>> Stashed changes
                 controller: "profileController",
                 controllerAs: "model",
                 resolve: {
                     user: checkLogin
                 }
             })
+<<<<<<< Updated upstream
             .when("/home", {
                 templateUrl: "views/templates/home.html",
                 controller: "homeController",
+=======
+            .when("/user/:uid/edit", {
+                templateUrl: "views/templates/profile.html",
+                controller: "profileController",
+>>>>>>> Stashed changes
                 controllerAs: "model",
                 resolve: {
                     user: checkLogin
@@ -44,6 +64,11 @@
                 controller: "songController",
                 controllerAs: "model"
             })
+            .when("/user/:uid/playlist/new", {
+                templateUrl: "views/templates/playlist-new.html",
+                controller: "playlistNewController",
+                controllerAs: "model"
+            });
 
         function checkLogin(userService, $q, $location) {
             var deferred = $q.defer();
