31f2295ccfdf82c6459353e6e53424111f23ad2f
liuchuhan <liu.chuh@husky.neu.edu>
2017-08-17 14:27:27 -0400

    add song to playlist move place

--- public/project/views/controller/explore.controller.client.js
+++ public/project/views/controller/explore.controller.client.js
@@ -6,14 +6,8 @@
     function exploreController(user, songService, playlistService, $location) {
         var model = this;
         model.user = user;
-        model.favourite = "dontlike";
-        model.playlistId = "";
         model.getAllSongs = getAllSongs;
         model.findAllCritics = findAllCritics;
-        model.addSongToPlaylist = addSongToPlaylist;
-        model.favouriteSong = favouriteSong;
-        model.dislike = dislike;
-        model.getSongCreator = getSongCreator;
         model.getPlaylist = getPlaylist;
 
         function init() {
@@ -34,29 +28,29 @@
             // model.reviews = reviewService.findAllReviews();
         }
 
-        function addSongToPlaylist() {
-            playlistService.addSongToPlaylist(model.playlistId, model.song._id)
-                .then(function (response) {
-                    $location.url('/explore');
-                })
-        }
-
-        function favouriteSong(song) {
-            model.favourite = "like";
-            model.song = song;
-            // console.log("song");
-            // console.log(model.song);
-
-        }
-
-        function getSongCreator() {
-            songService.getSongCreator(model.song._id)
-                .then(function (response) {
-                    // console.log(response.data);
-                    return model.creator = response.data;
-                })
-        }
-
+        // function addSongToPlaylist() {
+        //     playlistService.addSongToPlaylist(model.playlistId, model.song._id)
+        //         .then(function (response) {
+        //             $location.url('/explore');
+        //         })
+        // }
+        //
+        // function favouriteSong(song) {
+        //     model.favourite = "like";
+        //     model.song = song;
+        //     // console.log("song");
+        //     // console.log(model.song);
+        //
+        // }
+        //
+        // function getSongCreator() {
+        //     songService.getSongCreator(model.song._id)
+        //         .then(function (response) {
+        //             // console.log(response.data);
+        //             return model.creator = response.data;
+        //         })
+        // }
+        //
         function getPlaylist() {
             playlistService.findAllPlaylistsByUser(user._id)
                 .then(function (response) {
@@ -66,10 +60,10 @@
                     // console.log(model.playlists);
                 });
         }
-
-        function dislike() {
-            model.favourite = "dontlike";
-        }
+        //
+        // function dislike() {
+        //     model.favourite = "dontlike";
+        // }
     }
 
 
--- public/project/views/controller/song.controller.client.js
+++ public/project/views/controller/song.controller.client.js
@@ -3,16 +3,24 @@
         .module("Musiker")
         .controller("songController", songController);
 
-    function songController(songService, $routeParams, user) {
+    function songController(songService, playlistService, $routeParams,$location, user) {
         var model = this;
         model.user = user;
         model.findSongInfo = findSongInfo;
         model.addSong = addSong;
         model.reviewSong = reviewSong;
         model.modifySong = modifySong;
+        model.addSongToPlaylist = addSongToPlaylist;
+        model.favouriteSong = favouriteSong;
+        model.getSongCreator = getSongCreator;
+        model.getPlaylist = getPlaylist;
+
         var songId = $routeParams["songId"];
+        model.favourite = "no";
+        model.playlistId = "";
         function init() {
             findSongInfo();
+            getPlaylist();
         }
         init();
 
@@ -33,6 +41,36 @@
 
         function reviewSong(review) {
 
+        }
+
+        function addSongToPlaylist() {
+            playlistService.addSongToPlaylist(model.playlistId, songId)
+                .then(function (response) {
+                    $location.url('/explore');
+                })
+        }
+
+        function favouriteSong(option) {
+            model.favourite = option;
+
+        }
+
+        function getSongCreator() {
+            songService.getSongCreator(songId)
+                .then(function (response) {
+                    // console.log(response.data);
+                    return model.creator = response.data;
+                })
+        }
+
+        function getPlaylist() {
+            playlistService.findAllPlaylistsByUser(user._id)
+                .then(function (response) {
+                    // console.log(response);
+                    model.playlists = response.data;
+                    // console.log("model.playlists")
+                    // console.log(model.playlists);
+                });
         }
     }
 })();
